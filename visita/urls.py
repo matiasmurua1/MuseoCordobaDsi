c780548b102563c5786e4f98a743e3ce0f8e1b51
ToledoRodrigo <toledoarodrigo@gmail.com>
2021-06-27 03:03:13 -0300

    Added authentication

--- visita/urls.py
+++ visita/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
+
 from . import views
 
 urlpatterns = [
+    path("login/", auth_views.LoginView.as_view(template_name="login.html", authentication_form=AuthenticationForm), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="visita:index"), name="logout"),
     path("", views.index, name="index"),
 ]
